0fe89ea3bf88f43d7c6180902aba63c805b210a0
Keila Fernandes <keiladaianefernandes24@gmail.com>
2018-07-18 19:02:04 -0300

    Excluir / Validação

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function getTotal(lista){
 function setList(lista){
     let tabela = '<thead><tr><th scope="col">Item</th><th scope="col">Produto</th><th scope="col">Quantidade</th><th scope="col">Valor</th><th scope="col">Ação</th></tr></thead><tbody>';
     for(let chave in lista){
-        tabela += '<tr><th scope="row">' + chave + '</th><td>' + formatDesc(lista[chave].desc) + '</td><td>' + lista[chave].quantidade + '</td><td>' + formatValue(lista[chave].valor) + '</td><td> <button class="btn btn-primary" onclick="setUpdate(' + chave + ')">Editar</button> </td></tr>';
+        tabela += '<tr><th scope="row">' + chave + '</th><td>' + formatDesc(lista[chave].desc) + '</td><td>' + formQuantidade(lista[chave].quantidade) + '</td><td>' + formatValue(lista[chave].valor) + '</td><td> <button class="btn btn-primary" onclick="setUpdate(' + chave + ')">Editar</button>  <button class="btn btn-danger" onclick="deleteData(' + chave + ')">Excluir</button> </td></tr>';
     }
     tabela += '</tbody>';
     document.getElementById("lista-tabela").innerHTML = tabela;
@@ -37,12 +37,16 @@ function formatValue(value){
 }
 
 function addData(){
+    if(!validation()){
+        return;
+    }
     let desc = document.getElementById("desc").value;
     let quantidade = document.getElementById("quantidade").value;
     let valor = document.getElementById("valor").value;
 
     //uncshift - adiciona em primeiro na lista
     lista.unshift({"desc": desc, "quantidade": quantidade, "valor": valor})
+    resetForm();
     //atualiza lista
     setList(lista);
 }
@@ -65,9 +69,13 @@ function resetForm(){
     document.getElementById("btn-update").style.display = "none";
     document.getElementById("btn-add").style.display = "inline-block";
     document.getElementById("inputIdAtualizar").innerHTML = "";
+    document.getElementById("alert-validation").style.display = "none";
 }
 
 function updateData(){
+    if(!validation()){
+        return;
+    }
     let id = document.getElementById("idAtualizar").value;
     let desc = document.getElementById("desc").value;
     let quantidade = document.getElementById("quantidade").value;
@@ -76,6 +84,57 @@ function updateData(){
     lista[id] = { "desc": desc, "quantidade": quantidade, "valor": valor };
     resetForm();
     setList(lista); //Atualiza lista
+}
+
+function deleteData(id){
+    resetForm(); //para caso tenha algo editando na hora de apagar algo
+    if(confirm("Deletar este item?")){
+        if(id == lista.length - 1){
+            lista.pop(); //tira o último
+        }else if(id == 0){
+            lista.shift(); //tira o primeiro
+        }else{
+            let arrayAuxIni = lista.slice(0,id); //inicio até o id
+            let arrAuxEnd = lista.slice(id + 1); //do id em diante
+            lista = arrayAuxIni.concat(arrAuxEnd);
+        }
+        setList(lista);
+
+    }
+}
+
+function formQuantidade(valor){
+    return parseInt(valor);
+}
+
+function validation(){
+    let desc = document.getElementById("desc").value;
+    let quantidade = document.getElementById("quantidade").value;
+    let valor = document.getElementById("valor").value;
+    let errors = "";
+    document.getElementById("alert-validation").style.display = "none";
+    if(desc === ""){
+        errors += '<p>Preencha o campo da descrição do produto</p>';
+    }
+    if(quantidade === ""){
+        errors += '<p>Preencha o campo da quantidade do produto</p>';
+    }else if(quantidade != parseInt(quantidade)){
+        errors += '<p>Preencha o campo da quantidade do produto com uma quantidade válida</p>';
+    }
+    if(valor === ""){
+        errors += '<p>Preencha o campo do valor do produto</p>';
+    }else if(valor != parseFloat(valor)){
+        errors += '<p>Preencha o campo do valor do produto com um valor válido</p>';
+    }
+
+    if(errors != ""){
+        document.getElementById("errors").innerHTML = errors;
+        document.getElementById("alert-validation").style.display = "block";
+        return 0;
+    }else{
+        return 1;
+    }
+
 }
 
 setList(lista);
