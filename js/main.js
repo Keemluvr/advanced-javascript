7986f6daa1c418a1a1e5ef75e11c0edeecb813ee
Keila Fernandes <keiladaianefernandes24@gmail.com>
2018-07-17 21:07:43 -0300

    Atualizando dados

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function getTotal(lista){
 function setList(lista){
     let tabela = '<thead><tr><th scope="col">Item</th><th scope="col">Produto</th><th scope="col">Quantidade</th><th scope="col">Valor</th><th scope="col">Ação</th></tr></thead><tbody>';
     for(let chave in lista){
-        tabela += '<tr><th scope="row">' + chave + '</th><td>' + formatDesc(lista[chave].desc) + '</td><td>' + lista[chave].quantidade + '</td><td>' + formatValue(lista[chave].valor) + '</td> <td></td></tr>';
+        tabela += '<tr><th scope="row">' + chave + '</th><td>' + formatDesc(lista[chave].desc) + '</td><td>' + lista[chave].quantidade + '</td><td>' + formatValue(lista[chave].valor) + '</td><td> <button class="btn btn-primary" onclick="setUpdate(' + chave + ')">Editar</button> </td></tr>';
     }
     tabela += '</tbody>';
     document.getElementById("lista-tabela").innerHTML = tabela;
@@ -45,6 +45,37 @@ function addData(){
     lista.unshift({"desc": desc, "quantidade": quantidade, "valor": valor})
     //atualiza lista
     setList(lista);
+}
+
+function setUpdate(id){
+    let obj = lista[id];
+    document.getElementById("desc").value = obj.desc;
+    document.getElementById("quantidade").value = obj.quantidade;
+    document.getElementById("valor").value = obj.valor;
+    document.getElementById("btn-update").style.display = "inline-block";
+    document.getElementById("btn-add").style.display = "none";
+
+    document.getElementById("inputIdAtualizar").innerHTML = '<input id="idAtualizar" type="hidden" value="' + id + '">';
+}
+
+function resetForm(){
+    document.getElementById("desc").value = "";
+    document.getElementById("quantidade").value = "";
+    document.getElementById("valor").value = "";
+    document.getElementById("btn-update").style.display = "none";
+    document.getElementById("btn-add").style.display = "inline-block";
+    document.getElementById("inputIdAtualizar").innerHTML = "";
+}
+
+function updateData(){
+    let id = document.getElementById("idAtualizar").value;
+    let desc = document.getElementById("desc").value;
+    let quantidade = document.getElementById("quantidade").value;
+    let valor = document.getElementById("valor").value;
+
+    lista[id] = { "desc": desc, "quantidade": quantidade, "valor": valor };
+    resetForm();
+    setList(lista); //Atualiza lista
 }
 
 setList(lista);
